4471443474188ebb15bd13e59ce1228214a17bb4
wholooks <wholooks@users.noreply.github.com>
2021-08-14 20:21:26 +0200

    Add work class and setup tests

---
+++ pool/include/share.hpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2021 braidpool developers (see AUTHORS)
+ *
+ * This file is part of braidpool.
+ *
+ * braidpool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * braidpool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with braidpool.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SHARE_HPP
+#define SHARE_HPP
+
+#include <bitcoin/system.hpp>
+
+namespace bp {
+
+using namespace bc;
+using namespace bc::chain;
+using namespace bc::machine;
+using namespace bc::wallet;
+
+class share {
+};
+}
+
+#endif
